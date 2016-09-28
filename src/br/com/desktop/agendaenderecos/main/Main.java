29829f2e3b31c09f62840e599406898f52074ed1
Ramon Malaquias <malaquias@imd.ufrn.br>
2016-09-27 21:56:19 -0300

    Ajuste no dialog sobre

--- src/br/com/desktop/agendaenderecos/main/Main.java
+++ src/br/com/desktop/agendaenderecos/main/Main.java
@@ -28,229 +28,227 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
-	private Stage primaryStage;
-    private BorderPane rootLayout;
-    private ObservableList<Pessoa> listaPessoas = FXCollections.observableArrayList();
 
-    public Main(){
-    		listaPessoas = PessoaDAO.getPessoas();
-    }
-    
-    
-    public ObservableList<Pessoa> getListaPessoas() {
-		return listaPessoas;
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	private ObservableList<Pessoa> listaPessoas = FXCollections.observableArrayList();
+
+	public Main() {
+		listaPessoas = PessoaDAO.getPessoas();
 	}
 
+	public ObservableList<Pessoa> getListaPessoas() {
+		return listaPessoas;
+	}
 
 	public void setListaPessoas(ObservableList<Pessoa> listaPessoas) {
 		this.listaPessoas = listaPessoas;
 	}
 
-
 	@Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.getIcons().add(new Image("file:img/address_book.png"));
-        this.primaryStage.setTitle("Agenda de Endereços");
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.getIcons().add(new Image("file:img/address_book.png"));
+		this.primaryStage.setTitle("Agenda de Endereços");
 
-        initRootLayout();
+		initRootLayout();
 
-        showPessoaOverview();
-    }
+		showPessoaOverview();
+	}
 
-    /**
-     * Inicializa o root layout (layout base).
-     */
-    public void initRootLayout() {
-        try {
-            // Carrega o root layout do arquivo fxml.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class
-                    .getResource("../view/RootLayout.fxml"));
-            rootLayout = (BorderPane) loader.load();
+	/**
+	 * Inicializa o root layout (layout base).
+	 */
+	public void initRootLayout() {
+		try {
+			// Carrega o root layout do arquivo fxml.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
 
-            // Mostra a scene (cena) contendo o root layout.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
+			// Mostra a scene (cena) contendo o root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
 
-            // Dá ao controller o acesso ao main app.
-            RootLayoutController controller = loader.getController();
-            controller.setMain(this);
+			// Dá ao controller o acesso ao main app.
+			RootLayoutController controller = loader.getController();
+			controller.setMain(this);
 
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        // Tenta carregar o último arquivo de pessoa aberto.
-        File file = getPessoaFilePath();
-        if (file != null) {
-            loadPessoaDataFromFile(file);
-        }
-    }
+		// Tenta carregar o último arquivo de pessoa aberto.
+		File file = getPessoaFilePath();
+		if (file != null) {
+			loadPessoaDataFromFile(file);
+		}
+	}
 
-    /**
-     * Mostra o pessoa overview dentro do root layout.
-     */
-    public void showPessoaOverview() {
-    	 try {
-    	        // Carrega a pessoa overview.
-    	        FXMLLoader loader = new FXMLLoader();
-    	        loader.setLocation(Main.class.getResource("../view/PersonOverview.fxml"));
-    	        AnchorPane pessoaOverview = (AnchorPane) loader.load();
+	/**
+	 * Mostra o pessoa overview dentro do root layout.
+	 */
+	public void showPessoaOverview() {
+		try {
+			// Carrega a pessoa overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../view/PersonOverview.fxml"));
+			AnchorPane pessoaOverview = (AnchorPane) loader.load();
 
-    	        // Define a pessoa overview no centro do root layout.
-    	        rootLayout.setCenter(pessoaOverview);
+			// Define a pessoa overview no centro do root layout.
+			rootLayout.setCenter(pessoaOverview);
 
-    	        // Dá ao controlador acesso à the main app.
-    	        PessoaOverviewController controller = loader.getController();
-    	        controller.setMain(this);
+			// Dá ao controlador acesso à the main app.
+			PessoaOverviewController controller = loader.getController();
+			controller.setMain(this);
 
-    	    } catch (IOException e) {
-    	        e.printStackTrace();
-    	    }
-    }
-    /**
-     * Abre uma janela para editar detalhes para a pessoa especificada. Se o usuário clicar
-     * OK, as mudanças são salvasno objeto pessoa fornecido e retorna true.
-     * 
-     * @param pessoa O objeto pessoa a ser editado
-     * @return true Se o usuário clicou OK,  caso contrário false.
-     */
-    public boolean showPessoaEditDialog(Pessoa pessoa) {
-        try {
-            // Carrega o arquivo fxml e cria um novo stage para a janela popup.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("../view/PersonEditDialog.fxml"));
-            AnchorPane page = (AnchorPane) loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-            // Cria o palco dialogStage.
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("Editar Pessoa");
-            dialogStage.initModality(Modality.WINDOW_MODAL);
-            dialogStage.initOwner(primaryStage);
-            Scene scene = new Scene(page);
-            dialogStage.setScene(scene);
+	/**
+	 * Abre uma janela para editar detalhes para a pessoa especificada. Se o
+	 * usuário clicar OK, as mudanças são salvasno objeto pessoa fornecido e
+	 * retorna true.
+	 * 
+	 * @param pessoa
+	 *            O objeto pessoa a ser editado
+	 * @return true Se o usuário clicou OK, caso contrário false.
+	 */
+	public boolean showPessoaEditDialog(Pessoa pessoa) {
+		try {
+			// Carrega o arquivo fxml e cria um novo stage para a janela popup.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../view/PersonEditDialog.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
 
-            // Define a pessoa no controller.
-            PessoaEditDialogController controller = loader.getController();
-            controller.setDialogStage(dialogStage);
-            controller.setPessoa(pessoa);
+			// Cria o palco dialogStage.
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Editar Pessoa");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
 
-            // Mostra a janela e espera até o usuário fechar.
-            dialogStage.showAndWait();
+			// Define a pessoa no controller.
+			PessoaEditDialogController controller = loader.getController();
+			controller.setDialogStage(dialogStage);
+			controller.setPessoa(pessoa);
 
-            return controller.isOkClicked();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-    /**
-     * Retorna o arquivo de preferências da pessoa, o último arquivo que foi aberto.
-     * As preferências são lidas do registro específico do SO (Sistema Operacional). 
-     * Se tais prefêrencias não puderem  ser encontradas, ele retorna null.
-     * 
-     * @return
-     */
-    public File getPessoaFilePath() {
-        Preferences prefs = Preferences.userNodeForPackage(Main.class);
-        String filePath = prefs.get("filePath", null);
-        if (filePath != null) {
-            return new File(filePath);
-        } else {
-            return null;
-        }
-    }
+			// Mostra a janela e espera até o usuário fechar.
+			dialogStage.showAndWait();
 
-    /**
-     * Define o caminho do arquivo do arquivo carregado atual. O caminho é persistido no
-     * registro específico do SO (Sistema Operacional).
-     * 
-     * @param file O arquivo ou null para remover o caminho
-     */
-    public void setPessoaFilePath(File file) {
-        Preferences prefs = Preferences.userNodeForPackage(Main.class);
-        if (file != null) {
-            prefs.put("filePath", file.getPath());
+			return controller.isOkClicked();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-            // Update the stage title.
-            primaryStage.setTitle("AgendaEnderecos - " + file.getName());
-        } else {
-            prefs.remove("filePath");
+	/**
+	 * Retorna o arquivo de preferências da pessoa, o último arquivo que foi
+	 * aberto. As preferências são lidas do registro específico do SO (Sistema
+	 * Operacional). Se tais prefêrencias não puderem ser encontradas, ele
+	 * retorna null.
+	 * 
+	 * @return
+	 */
+	public File getPessoaFilePath() {
+		Preferences prefs = Preferences.userNodeForPackage(Main.class);
+		String filePath = prefs.get("filePath", null);
+		if (filePath != null) {
+			return new File(filePath);
+		} else {
+			return null;
+		}
+	}
 
-            // Update the stage title.
-            primaryStage.setTitle("Agenda de Endereços");
-        }
-    }
-    
-    /**
-     * Carrega os dados da pessoa do arquivo especificado. A pessoa atual
-     * será substituída.
-     * 
-     * @param file
-     */
-    public void loadPessoaDataFromFile(File file) {
-        try {
-            JAXBContext context = JAXBContext
-                    .newInstance(PessoaListWrapper.class);
-            Unmarshaller um = context.createUnmarshaller();
+	/**
+	 * Define o caminho do arquivo do arquivo carregado atual. O caminho é
+	 * persistido no registro específico do SO (Sistema Operacional).
+	 * 
+	 * @param file
+	 *            O arquivo ou null para remover o caminho
+	 */
+	public void setPessoaFilePath(File file) {
+		Preferences prefs = Preferences.userNodeForPackage(Main.class);
+		if (file != null) {
+			prefs.put("filePath", file.getPath());
 
-            // Reading XML from the file and unmarshalling.
-            PessoaListWrapper wrapper = (PessoaListWrapper) um.unmarshal(file);
+			// Update the stage title.
+			primaryStage.setTitle("AgendaEnderecos - " + file.getName());
+		} else {
+			prefs.remove("filePath");
 
-            listaPessoas.clear();
-            listaPessoas.addAll(wrapper.getPessoas());
+			// Update the stage title.
+			primaryStage.setTitle("Agenda de Endereços");
+		}
+	}
 
-            // Save the file path to the registry.
-            setPessoaFilePath(file);
+	/**
+	 * Carrega os dados da pessoa do arquivo especificado. A pessoa atual será
+	 * substituída.
+	 * 
+	 * @param file
+	 */
+	public void loadPessoaDataFromFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PessoaListWrapper.class);
+			Unmarshaller um = context.createUnmarshaller();
 
-        } catch (Exception e) { // catches ANY exception
-            Dialogs.create()
-                    .title("Erro")
-                    .masthead("Não foi possível carregar dados do arquivo:\n" 
-                              + file.getPath()).showException(e);
-        }
-    }
+			// Reading XML from the file and unmarshalling.
+			PessoaListWrapper wrapper = (PessoaListWrapper) um.unmarshal(file);
 
-    /**
-     * Salva os dados da pessoa atual no arquivo especificado.
-     * 
-     * @param file
-     */
-    public void savePessoaDataToFile(File file) {
-        try {
-            JAXBContext context = JAXBContext
-                    .newInstance(PessoaListWrapper.class);
-            Marshaller m = context.createMarshaller();
-            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			listaPessoas.clear();
+			listaPessoas.addAll(wrapper.getPessoas());
 
-            // Envolvendo nossos dados da pessoa.
-            PessoaListWrapper wrapper = new PessoaListWrapper();
-            wrapper.setPessoas(listaPessoas);
+			// Save the file path to the registry.
+			setPessoaFilePath(file);
 
-            // Enpacotando e salvando XML  no arquivo.
-            m.marshal(wrapper, file);
+		} catch (Exception e) { // catches ANY exception
+			Dialogs.create().title("Erro").masthead("Não foi possível carregar dados do arquivo:\n" + file.getPath())
+					.showException(e);
+		}
+	}
 
-            // Saalva o caminho do arquivo no registro.
-            setPessoaFilePath(file);
-        } catch (Exception e) { // catches ANY exception
-            Dialogs.create().title("Erro")
-                    .masthead("Não foi possível salvar os dados do arquivo:\n" 
-                              + file.getPath()).showException(e);
-        }
-    }    
-    
-    /**
-     * Retorna o palco principal.
-     * @return
-     */
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
+	/**
+	 * Salva os dados da pessoa atual no arquivo especificado.
+	 * 
+	 * @param file
+	 */
+	public void savePessoaDataToFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PessoaListWrapper.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+			// Envolvendo nossos dados da pessoa.
+			PessoaListWrapper wrapper = new PessoaListWrapper();
+			wrapper.setPessoas(listaPessoas);
+
+			// Enpacotando e salvando XML no arquivo.
+			m.marshal(wrapper, file);
+
+			// Saalva o caminho do arquivo no registro.
+			setPessoaFilePath(file);
+		} catch (Exception e) { // catches ANY exception
+			Dialogs.create().title("Erro").masthead("Não foi possível salvar os dados do arquivo:\n" + file.getPath())
+					.showException(e);
+		}
+	}
+
+	/**
+	 * Retorna o palco principal.
+	 * 
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- src/br/com/desktop/agendaenderecos/view/RootLayoutController.java
+++ src/br/com/desktop/agendaenderecos/view/RootLayoutController.java
@@ -1,117 +1,119 @@
 package br.com.desktop.agendaenderecos.view;
-import java.io.File;
 
-import org.controlsfx.dialog.Dialogs;
+import java.io.File;
 
 import br.com.desktop.agendaenderecos.main.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 
 /**
- * O controlador para o root layout. O root layout provê um layout básico
- * para a aplicação contendo uma barra de menu e um espaço onde outros elementos
- * JavaFX podem ser colocados.
+ * O controlador para o root layout. O root layout provê um layout básico para a
+ * aplicação contendo uma barra de menu e um espaço onde outros elementos JavaFX
+ * podem ser colocados.
  * 
  * @author Marco Jakob
  */
 public class RootLayoutController {
 
-    // Referência à aplicação principal
-    private Main main;
+	// Referência à aplicação principal
+	private Main main;
 
-    /**
-     * É chamado pela aplicação principal para referenciar a si mesma.
-     * 
-     * @param main
-     */
-    public void setMain(Main main) {
-        this.main = main;
-    }
+	/**
+	 * É chamado pela aplicação principal para referenciar a si mesma.
+	 * 
+	 * @param main
+	 */
+	public void setMain(Main main) {
+		this.main = main;
+	}
 
-    /**
-     * Cria uma agenda vazia.
-     */
-    @FXML
-    private void handleNew() {
-        main.getListaPessoas().clear();
-        main.setPessoaFilePath(null);
-    }
+	/**
+	 * Cria uma agenda vazia.
+	 */
+	@FXML
+	private void handleNew() {
+		main.getListaPessoas().clear();
+		main.setPessoaFilePath(null);
+	}
 
-    /**
-     * Abre o FileChooser para permitir o usuário selecionar uma agenda
-     * para carregar.
-     */
-    @FXML
-    private void handleOpen() {
-        FileChooser fileChooser = new FileChooser();
+	/**
+	 * Abre o FileChooser para permitir o usuário selecionar uma agenda para
+	 * carregar.
+	 */
+	@FXML
+	private void handleOpen() {
+		FileChooser fileChooser = new FileChooser();
 
-        // Define um filtro de extensão
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-        fileChooser.getExtensionFilters().add(extFilter);
+		// Define um filtro de extensão
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+		fileChooser.getExtensionFilters().add(extFilter);
 
-        // Mostra a janela de salvar arquivo
-        File file = fileChooser.showOpenDialog(main.getPrimaryStage());
+		// Mostra a janela de salvar arquivo
+		File file = fileChooser.showOpenDialog(main.getPrimaryStage());
 
-        if (file != null) {
-            main.loadPessoaDataFromFile(file);
-        }
-    }
+		if (file != null) {
+			main.loadPessoaDataFromFile(file);
+		}
+	}
 
-    /**
-     * Salva o arquivo para o arquivo de pessoa aberto atualmente. Se não houver
-     * arquivo aberto, a janela "salvar como" é mostrada.
-     */
-    @FXML
-    private void handleSave() {
-        File pessoaFile = main.getPessoaFilePath();
-        if (pessoaFile != null) {
-            main.savePessoaDataToFile(pessoaFile);
-        } else {
-            handleSaveAs();
-        }
-    }
+	/**
+	 * Salva o arquivo para o arquivo de pessoa aberto atualmente. Se não houver
+	 * arquivo aberto, a janela "salvar como" é mostrada.
+	 */
+	@FXML
+	private void handleSave() {
+		File pessoaFile = main.getPessoaFilePath();
+		if (pessoaFile != null) {
+			main.savePessoaDataToFile(pessoaFile);
+		} else {
+			handleSaveAs();
+		}
+	}
 
-    /**
-     * Abre um FileChooser para permitir o usuário selecionar um arquivo
-     * para salvar.
-     */
-    @FXML
-    private void handleSaveAs() {
-        FileChooser fileChooser = new FileChooser();
+	/**
+	 * Abre um FileChooser para permitir o usuário selecionar um arquivo para
+	 * salvar.
+	 */
+	@FXML
+	private void handleSaveAs() {
+		FileChooser fileChooser = new FileChooser();
 
-        // Define o filtro de extensão
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-        fileChooser.getExtensionFilters().add(extFilter);
+		// Define o filtro de extensão
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+		fileChooser.getExtensionFilters().add(extFilter);
 
-        // Mostra a janela para salvar arquivo
-        File file = fileChooser.showSaveDialog(main.getPrimaryStage());
+		// Mostra a janela para salvar arquivo
+		File file = fileChooser.showSaveDialog(main.getPrimaryStage());
 
-        if (file != null) {
-            // Certifica de que esta é a extensão correta
-            if (!file.getPath().endsWith(".xml")) {
-                file = new File(file.getPath() + ".xml");
-            }
-            main.savePessoaDataToFile(file);
-        }
-    }
+		if (file != null) {
+			// Certifica de que esta é a extensão correta
+			if (!file.getPath().endsWith(".xml")) {
+				file = new File(file.getPath() + ".xml");
+			}
+			main.savePessoaDataToFile(file);
+		}
+	}
 
-    /**
-     * Abre uma janela Sobre.
-     */
-    @FXML
-    private void handleAbout() {
-        Dialogs.create()
-            .title("AddressApp")
-            .masthead("Sobre")
-            .message("Autor: Marco Jakob\nWebsite: http://code.makery.ch")
-            .showInformation();
-    }
+	/**
+	 * Abre uma janela Sobre.
+	 */
+	@FXML
+	private void handleAbout() {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Agenda de Endereços");
+		alert.setHeaderText("Sobre");
+		alert.setContentText("Autor: Marco Jakob\nWebsite: http://code.makery.ch\nAdapted by: Ramon Santos");
+		alert.showAndWait();
+	}
 
-    /**
-     * Fecha a aplicação.
-     */
-    @FXML
-    private void handleExit() {
-        System.exit(0);
-    }
+	/**
+	 * Fecha a aplicação.
+	 */
+	@FXML
+	private void handleExit() {
+		System.exit(0);
+	}
 }
